7b3841a9cd29be8ee1ec998116430f81c46cf07d
WFSraboet <67421033+WFSraboet@users.noreply.github.com>
2020-06-26 13:35:29 +0700

    Update validationcancelclient.js

--- js/validationcancelclient.js
+++ js/validationcancelclient.js
@@ -1,5 +1,5 @@
 function buttoncancelclient(){
-	var r = confirm("Apakah anda yakin?");
+	var r = confirm("Apakah anda yakin ingin membatalkan pekerjaan?");
 	if(r == true){
 		alert("Pekerjaan dibatalkan!");
 		location.replace("sraboetclienthomepage.html");
