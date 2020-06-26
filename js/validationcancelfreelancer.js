7f8c9ac7903d0fb58be4349cff1f28fabd46f91e
WFSraboet <67421033+WFSraboet@users.noreply.github.com>
2020-06-26 13:35:54 +0700

    Update validationcancelfreelancer.js

--- js/validationcancelfreelancer.js
+++ js/validationcancelfreelancer.js
@@ -1,5 +1,5 @@
 function buttoncancelfreelancer(){
-	var r = confirm("Apakah anda yakin?");
+	var r = confirm("Apakah anda yakin ingin membatalkan pekerjaaan?");
 	if(r == true){
 		alert("Pekerjaan dibatalkan!");
 		location.replace("sraboetfreelancerhomepage.html");
